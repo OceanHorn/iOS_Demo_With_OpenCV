b827218b22e3264938f52a00effd90c78ff30e04
郭玉富 <OceanHorn@163.com>
2017-06-05 21:09:50 +0800

    实现文件调用

--- OpenCVDemo/ViewController.m
+++ OpenCVDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "UIImage+Change.h"
 
 @interface ViewController ()
 
@@ -17,8 +18,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self setupUIComponents];
 }
 
+- (void)setupUIComponents {
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 20, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height/3.0)];
+    UIImage *image = [UIImage imageNamed:@"road.jpeg"];
+    imageView.image = image;
+    
+    [self.view addSubview:imageView];
+    
+    UIImageView *imageView1 = [[UIImageView alloc] initWithFrame:CGRectMake(0, 250, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height/3.0)];
+    UIImage *image1 = [UIImage imageNamed:@"road.jpeg"];
+    imageView1.image = [image1 changeImage];
+    
+    [self.view addSubview:imageView1];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
